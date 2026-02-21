c28840fe2d96c55d35f286c3b8702a35753f9e2f
Pratham Javalkoti <prathamsjavalkoti@gmail.com>
2026-02-21 08:39:00 +0530

    Created react app, understood src/index.html file, scr/main.jsx file and src/App.jsx file.

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+function MySelf() {
+  return (
+    <>
+      <h1>Hello this is me Pratham!</h1>
+      <p>I am the best and I know it!</p>
+    </>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <StrictMode>
+      <App />
+      <MySelf />
+    </StrictMode>
+  </>
+)
