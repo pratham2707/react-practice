4ad9ae121e1f13fb3a277afd8331880539783cf5
Pratham Javalkoti <prathamsjavalkoti@gmail.com>
2026-04-02 06:55:19 +0530

    Sass modules (color module)

--- src/Components/Sass/MyHeader.jsx
+++ src/Components/Sass/MyHeader.jsx
@@ -2,7 +2,11 @@ import './MyStyle.scss';
 
 function MyHeader() {
     return (
-        <h1>My Header</h1>
+        <>
+            <h1>My Header</h1>
+            <h2>My Header with less brighter color</h2>
+            <h3>My Header with brighter color</h3>
+        </>
     );
 }
 
